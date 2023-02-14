3d247bf7e27a6a0bf66348c8c7d8a7d48986999a
Arturs Janeks <94831085+ajaneks@users.noreply.github.com>
2023-02-14 10:13:34 +0200

    save funkcija

--- kalendars3.py
+++ kalendars3.py
@@ -3,12 +3,15 @@ from tkinter import ttk
 from tkcalendar import Calendar
 import datetime
 import customtkinter
+import json
+
+
 
 # Objekta izveide
-# root = Tk()
 root = customtkinter.CTk()
+root.title("Plānotājs")
 
-root.geometry("400x600")
+root.geometry("500x600")
 
 customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -17,24 +20,19 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 # Iegut sodienas datumu
 today = datetime.datetime.today()
 
-# notebook = ttk.Notebook(root)
-# notebook.pack(fill = BOTH, expand = True)
-
-# # Kalendara tabs
-# calendar_tab = ttk.Frame(notebook)
-# notebook.add(calendar_tab, text = "Calendar")
-
-# # Komentaru tabs
-# comments_tab = ttk.Frame(notebook)
-# notebook.add(comments_tab, text = "Comments")
 
 def grad_date(event):
     selected_date = cal.get_date()
     if selected_date in comments:
         comment = comments[selected_date]
+        # Set background color to yellow for dates with comments
+        cal.tag_config(selected_date, background='yellow')
     else:
-        comment = "No comment for this date."
-    date.config(text = "Selected Date is: " + selected_date + "\nComment: " + comment)
+        comment = "Šajā datumā nav plānu."
+        # Set default background color for dates without comments
+        cal.tag_config(selected_date, background='white')
+    date.config(text = "Izvēlētais datums ir: " + selected_date + "\nPlāni: " + comment)
+
 
 # Create the calendar
 calendar_frame = Frame(root)
@@ -47,57 +45,71 @@ cal.bind("<<CalendarSelected>>", grad_date)
 comments_frame = Frame(root)
 comments_frame.pack(side=RIGHT, fill=BOTH, expand=True)
 
+# Kaste, kur uzradas komentari
+comments_list = Listbox(comments_frame)
+comments_list.pack(fill = BOTH, expand = True)
+
+
 def display_comments():
     comments_list.delete(0, END)
     for date, comment in comments.items():
         comments_list.insert(END, date + ": " + comment)
 
-# comments_list = Listbox(comments_frame)
-# comments_list.pack(fill=BOTH, expand=True)
-
-
-
-
-# # Kalendara pievienosana
-# cal = Calendar(calendar_tab, selectmode = 'day', year = today.year, month = today.month, day = today.day)
-# cal.pack(pady = 20)
-# cal.bind("<<CalendarSelected>>", grad_date)
-
 # Komentaru un datumu vieta
 comments = {}
 
+for date in comments.keys():
+    cal.tag_config(date, background='yellow')
+
+# Save comments to file
+def save_comments():
+    with open('comments.txt', 'w') as file:
+        for date, comment in comments.items():
+            file.write(f'{date}:{comment}\n')
+
+# Load comments from file
+def load_comments():
+    try:
+        with open('comments.txt', 'r') as file:
+            for line in file:
+                date, comment = line.strip().split(':')
+                comments[date] = comment
+    except FileNotFoundError:
+        pass
+
+load_comments() # load saved comments on startup
+display_comments() # display comments on startup
 
 def add_comment():
     selected_date = cal.get_date()
     comment = comment_entry.get()
     comments[selected_date] = comment
     comment_entry.delete(0, END)
-    date.config(text = "Selected Date is: " + selected_date + "\nComment: " + comment)
+    date.config(text = "Izvēlētais datums ir: " + selected_date + "\nPlāni: " + comment)
     display_comments()
+    # Set background color to yellow for dates with comments
+    cal.tag_config(selected_date, background='yellow')
+    save_comments() # save comments after adding
 
 def remove_comment():
     selected_date = cal.get_date()
     if selected_date in comments:
         del comments[selected_date]
-        date.config(text = "Comment removed for: " + selected_date)
+        date.config(text = "Noņemt plānu no: " + selected_date)
         display_comments()
+        # Set default background color for dates without comments
+        cal.tag_config(selected_date, background='white')
+        save_comments() # save comments after removing
     else:
-        date.config(text = "No comment to remove for: " + selected_date)
+        date.config(text = "Šajā datumā nav plānu: " + selected_date)
+
+
+
+
 
 # Pogas
-customtkinter.CTkButton(calendar_frame, text = "Add Comment", command = add_comment).pack(pady = 20)
-customtkinter.CTkButton(calendar_frame, text = "Remove Comment", command = remove_comment).pack(pady = 20)
-
-segemented_button_var = customtkinter.StringVar(value="Kalendārs")
-segemented_button_var = customtkinter.StringVar(value="Plāni")  # set initial value
-
-segemented_button = customtkinter.CTkSegmentedButton(master=root,
-                                                     values=["Kalendārs","Plāni"],
-                                                     variable=segemented_button_var)
-segemented_button.pack(padx=20, pady=10)
-
-# button = customtkinter.CTkButton(master=root, text="Get Date", command=grad_date)
-# button.place(relx=0.5, rely=0.5)
+customtkinter.CTkButton(calendar_frame, text = "Pievienot plānu", command = add_comment).pack(pady = 20)
+customtkinter.CTkButton(calendar_frame, text = "Dzēst plānu", command = remove_comment).pack(pady = 20)
 
 comment_entry = Entry(calendar_frame)
 comment_entry.pack(pady = 20)
@@ -105,14 +117,6 @@ comment_entry.pack(pady = 20)
 date = Label(calendar_frame, text = "")
 date.pack(pady = 20)
 
-# Kaste, kur uzradas komentari
-def display_comments():
-    comments_list.delete(0, END)
-    for date, comment in comments.items():
-        comments_list.insert(END, date + ": " + comment)
-
-comments_list = Listbox(comments_frame)
-comments_list.pack(fill = BOTH, expand = True)
 
 
 root.mainloop()
